3e3096ae26b817bc6ff00082788716e6c71e7916
Seb8263 <sebachomedinav22@hotmail.com>
2021-11-01 22:08:01 -0300

    tarea terminada

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -138,7 +138,7 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
-  var suma  = 0
+  var suma  = 0;
   for ( var i = 0; i < usuario.posts.length; i++) {
     suma = suma +  usuario.posts[i].likes;
   }
